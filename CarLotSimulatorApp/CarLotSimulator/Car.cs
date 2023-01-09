08ac6074b1a412195d706e13c044e68dc5424bb2
ross dibartolo <dibartoloross@gmail.com>
2023-01-09 14:35:53 -0500

    initial commit

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDriveable { get; set; }
+
+        public void MakeEngineNoise(string EngineNoise)
+        {
+            Console.WriteLine($"This is the sound of an engine {EngineNoise}.");
+        }
+        public void MakeHonkNoise(string HonkNoise)
+        {
+            Console.WriteLine($"This is the sound of a horn{HonkNoise}.");
+        }
+
+        public void CarType()
+        {
+            Console.WriteLine($"{Year} {Make} {Model}");
+        }
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CarLotSimulator
 {
@@ -6,6 +7,8 @@ namespace CarLotSimulator
     {
         static void Main(string[] args)
         {
+            var rossCarLot = new CarLot();
+            
             //TODO
 
             //Create a seperate class file called Car
@@ -17,10 +20,43 @@ namespace CarLotSimulator
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
             //Call each of the methods for each car
+            var wrangler = new Car();
+            wrangler.Make = "Jeep";
+            wrangler.Model = "Wrangler";
+            wrangler.Year = 2023;
+            rossCarLot.carList.Add(wrangler);
+            //wrangler.CarType();
+
+            //var viper = new Car();
+            //viper.Make = "Dodge";
+            //viper.Model = "Viper";
+            //viper.Year = 2001;
+
+            //viper.CarType();
+
+            //var corvette = new Car();
+            //corvette.Make = "Chevy";
+            //corvette.Model = "Stringray";
+            //corvette.Year = 2023;
 
             //*************BONUS*************//
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
+            //var wrangler = new Car();
+            //wrangler.Make = "Jeep";
+            //wrangler.Model = "Wrangler";
+            //wrangler.Year = 2023;
+
+            var viper = new Car("Dodge", "Viper", 2001);
+            rossCarLot.carList.Add(viper);
+
+            var corvette = new Car()
+            {
+                Make = "Chevy",
+                Model = "Stingray",
+                Year = 2023
+            };
+            rossCarLot.carList.Add(corvette);
 
             //*************BONUS X 2*************//
 
@@ -28,6 +64,11 @@ namespace CarLotSimulator
             //It should have at least one property: a List of cars
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+            var carList = new List<Car>() { wrangler, viper, corvette };
+            foreach(var car in carList)
+            {
+                Console.WriteLine($"{car.Year} {car.Make} {car.Model}");
+            }
         }
     }
 }
